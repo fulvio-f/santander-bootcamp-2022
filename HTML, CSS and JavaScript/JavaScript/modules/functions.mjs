f884d8fa6a25bda3423e49230bbe6d6cb0a6ce23
Fulvio Favilla <fulvio.f@usp.br>
2022-07-27 17:13:26 -0300

    Studying JavaScript (DOM).

---
+++ HTML, CSS and JavaScript/JavaScript/modules/functions.mjs
@@ -0,0 +1,17 @@
+function showAge(name, age) {
+    return `The age of ${name} is ${age}.`;
+}
+
+function showCity(name, city) {
+    return `The city of ${name} is ${city}.`;
+}
+
+function showHobby(name, hobby) {
+    return `The hobby of ${name} is ${hobby}.`;
+}
+
+export {
+    showAge,
+    showCity,
+    showHobby
+}
---
+++ HTML, CSS and JavaScript/JavaScript/modules/main.mjs
@@ -0,0 +1,5 @@
+import { showAge, showCity, showHobby } from "./functions.mjs";
+
+console.log(showAge("Fulvio", 23));
+console.log(showCity("Fulvio", "São Paulo"));
+console.log(showHobby("Fulvio", "playing Chess"));
